14ed02b47e3f4621fde60919e1782b76d15f9b38
DucDai <phducdai47@gmail.com>
2024-05-27 18:50:50 +0700

    imp feat notify

---
+++ be/src/main/java/com/matcha/nlulibrary/dao/NotifyDao.java
@@ -0,0 +1,10 @@
+package com.matcha.nlulibrary.dao;
+
+import com.matcha.nlulibrary.entity.NotifyEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface NotifyDao extends JpaRepository<NotifyEntity, String> {
+    List<NotifyEntity> findAll();
+}
--- be/src/main/java/com/matcha/nlulibrary/entity/NotifyEntity.java
+++ be/src/main/java/com/matcha/nlulibrary/entity/NotifyEntity.java
@@ -2,6 +2,7 @@ package com.matcha.nlulibrary.entity;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -12,6 +13,7 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 @Entity
+@Table(name = "notify")
 public class NotifyEntity {
     @Id
     private String id;
