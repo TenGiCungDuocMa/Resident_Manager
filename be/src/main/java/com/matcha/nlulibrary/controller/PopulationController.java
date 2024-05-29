0ef075c3c3252618eb957cb2cb4e57eb0d2d18a5
WinTranIT <21130536@st.hcmuaf.edu.vn>
2024-05-29 15:55:01 +0700

    add comment

--- be/src/main/java/com/matcha/nlulibrary/controller/PopulationController.java
+++ be/src/main/java/com/matcha/nlulibrary/controller/PopulationController.java
@@ -12,16 +12,23 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/population")
 @RequiredArgsConstructor
+//đây là lớp PopulationController trong sequence
+// của chức năng tóm tắt thông tin ở trang chủ
 public class PopulationController {
     @Autowired
     private PopulationService populationService;
 
     @GetMapping("/years")
+    // phương thức này sẽ trả về danh sách các năm bằng cách
+    // thực hiện phương thức getAllYears trong lớp PopulationService
     public List<Integer> getAllYears() {
         return populationService.getAllYears();
     }
 
     @GetMapping("/numbers")
+    // phương thức này sẽ trả về danh sách số dân bằng cách
+    // thực hiện phương thức getAllPopulations trong lớp PopulationService
+    //phương thức này để lấy ra số dân tương ứng với mỗi năm trong csdl
     public List<Long> getAllPopulations() {
         return populationService.getAllPopulations();
     }
